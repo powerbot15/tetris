ccbe44abf7796a820c72f73d0040e4efcbdbc21c
oleh.kastornov <powerbot15@gmail.com>
2014-10-25 23:13:44 +0300

    figure controller

--- js/fieldController.js
+++ js/fieldController.js
@@ -4,6 +4,8 @@ define(['jquery', 'figure'], function($, Figure){
         this.fieldCells = new Array(20);
 
         this.init();
+        this.figure = new Figure();
+        this.paintFigure();
         console.log('Field created');
     }
 
@@ -15,20 +17,42 @@ define(['jquery', 'figure'], function($, Figure){
             for(var j = 0; j < 10; j++){
                 this.fieldCells[i].push({
                     cell : cells.eq(cellCounter),
-                    active : false
+                    active : 0
                 });
                 cellCounter++;
             }
         }
-
-        for(var i = 0; i < 10; i++){
-            this.fieldCells[0][i].cell.addClass('active');
-        }
-        for(var i = 0; i < 20; i++){
-            this.fieldCells[i][0].cell.addClass('active');
-        }
+        this.refreshField();
+        //for(var i = 0; i < 10; i++){
+        //    this.fieldCells[0][i].cell.addClass('active');
+        //}
+        //for(var i = 0; i < 20; i++){
+        //    this.fieldCells[i][0].cell.addClass('active');
+        //}
 
     };
+    FieldController.prototype.refreshField = function(){
+        for(var i = 0; i < this.fieldCells.length; i++){
+            for(var j = 0; j < this.fieldCells[i].length; j++){
+                if(this.fieldCells[i][j].active){
+                    this.fieldCells[i][j].cell.addClass('active');
+                }
+                else{
+                    this.fieldCells[i][j].cell.removeClass('active');
+                }
+            }
+        }
+    };
+    FieldController.prototype.paintFigure = function(){
+        var figureCoords = this.figure.getCoordinates();
 
+        for(var i = 0; i < figureCoords.length; i++){
+            for(var j = 0; j < figureCoords.length; j++){
+                if(figureCoords[i][j].active){
+                    this.fieldCells[figureCoords[i][j].y][figureCoords[i][j].x].cell.addClass('active');
+                }
+            }
+        }
+    };
     return FieldController;
 });
--- js/figure.js
+++ js/figure.js
@@ -1,9 +1,98 @@
 define(['jquery'],function($){
+    var FIGURE_PRIMITIVES = {
+        square : {
+            topLeft : {x : 4, y : 0},
+            points : [
+                [1, 1],
+                [1, 1]
+            ]
+        },
 
+        line : {
+            topLeft : {x : 3, y : 0},
+            points : [
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 0, 0]
+            ]
+        },
+        trident : {
+            topLeft : {x : 3, y : 0},
+            points : [
+                [0, 1, 0],
+                [0, 1, 1],
+                [0, 1, 0]
+            ]
+        },
+        leftUpBlock : {
+            topLeft : {x : 4, y : 0},
+            points : [
+                [1, 0, 0],
+                [1, 1, 0],
+                [0, 1, 0]
+            ]
+
+        },
+        rightUpBlock : {
+            topLeft : {x : 4, y : 0},
+            points : [
+                [0, 1, 0],
+                [1, 1, 0],
+                [1, 0, 0]
+            ]
+        }
+
+    };
     function Figure(){
-
+        this.figure = FIGURE_PRIMITIVES.trident;
+        this.rotateMatrix = new Array(this.figure.points.length);
+        for(var i = 0; i < this.rotateMatrix.length; i++){
+            this.rotateMatrix[i] = new Array(this.figure.points.length);
+            for(var j = 0; j < this.rotateMatrix[i].length; j++){
+                this.rotateMatrix[i][j] = 0;
+            }
+        }
     }
 
+    Figure.prototype.rotate = function(){
+        var length = this.figure.points.length;
+        //for(var i = 0; i < this.figure.points.length; i++){
+        //    for(var j = 0; j < this.figure.points.length; j++){
+        //        this.rotateMatrix[j][i] = this.figure.points[i][j];
+        //    }
+        //}
+        //console.dir(this.rotateMatrix);
+        for(var i=0;i<length;i++){
+            for(var j=0;j<length;j++){
+                this.rotateMatrix[j][length-1-i] = this.figure.points[i][j];
+            }
+        }
+        for(var i = 0; i < length; i++){
+            for(var j = 0; j < length; j++){
+                this.figure.points[i][j] = this.rotateMatrix[i][j];
+            }
+        }
+
+        //console.dir(this.rotateMatrix);
+        //TNEW[j][n-1-i]=TOLD[i][j];
+    };
+
+    Figure.prototype.getCoordinates = function(){
+        var coordinates = new Array(this.figure.points.length);
+
+        for(var i = 0; i < coordinates.length; i++){
+            coordinates[i] = new Array(this.figure.points.length);
+            for(var j = 0; j < coordinates[i].length; j++){
+                coordinates[i][j] = {
+                    active : this.figure.points[i][j],
+                    x : this.figure.topLeft.x + j,
+                    y : this.figure.topLeft.y + i
+                };
+            }
+        }
+        return coordinates;
+    };
     return Figure;
 
 });

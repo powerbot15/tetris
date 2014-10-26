83fb7682bc1c36ca7a141a7a311df572f5afcca5
oleh.kastornov <powerbot15@gmail.com>
2014-10-26 17:47:39 +0200

    nearly done

--- js/fieldController.js
+++ js/fieldController.js
@@ -1,12 +1,17 @@
-define(['jquery', 'figure'], function($, Figure){
+define(['jquery', 'figure'], function ($, Figure){
 
     function FieldController(){
         this.fieldCells = new Array(20);
-
+        this.speed = 10;
+        this.speedHimanize = 1;
+        this.speedCounter = 0;
+        this.currentStage = 10000;
         this.init();
+        this.playerScore = 0;
         this.figure = new Figure();
         this.paintFigure();
         console.log('Field created');
+        this.handleKeyMoves();
     }
 
     FieldController.prototype.init = function (){
@@ -23,10 +28,11 @@ define(['jquery', 'figure'], function($, Figure){
             }
         }
         this.refreshField();
-
+        //this.checkCollectedLines();
+        this.gameCycleStart();
     };
 
-    FieldController.prototype.refreshField = function(){
+    FieldController.prototype.refreshField = function (){
         for(var i = 0; i < this.fieldCells.length; i++){
             for(var j = 0; j < this.fieldCells[i].length; j++){
                 if(this.fieldCells[i][j].active){
@@ -39,17 +45,196 @@ define(['jquery', 'figure'], function($, Figure){
         }
     };
 
-    FieldController.prototype.clearFigure = function(){
+    FieldController.prototype.clearFigure = function (){
       this.paintFigure({clear : true});
     };
 
-    FieldController.prototype.paintFigure = function(options){
+    FieldController.prototype.paintFigure = function (options){
         var figureCoords = this.figure.getCoordinates(),
             classMethod = options && options.clear ? 'removeClass' : 'addClass';
 
         for(var i = 0; i < figureCoords.length; i++){
             this.fieldCells[figureCoords[i].y][figureCoords[i].x].cell[classMethod]('active');
+            this.fieldCells[figureCoords[i].y][figureCoords[i].x].active = !this.fieldCells[figureCoords[i].y][figureCoords[i].x].active;
         }
+    };
+
+    FieldController.prototype.canFigureMove = function (direction){
+
+        var coordinatesToCheck = this.figure.getCoordinates();
+        this.clearFigure();
+        for(var i = 0; i < coordinatesToCheck.length; i++){
+            if(direction === 'left'){
+                coordinatesToCheck[i].x--;
+                if(coordinatesToCheck[i].x < 0){
+                    this.paintFigure();
+                    return false;
+                }
+            }
+            if(direction === 'right'){
+                coordinatesToCheck[i].x++;
+                if(coordinatesToCheck[i].x > 9){
+                    this.paintFigure();
+                    return false;
+                }
+            }
+            if(direction === 'down'){
+               if(++coordinatesToCheck[i].y > 19){
+                   this.paintFigure();
+                   //this.figure = new Figure();
+                   return false;
+               }
+            }
+            if(direction === 'new'){
+                //check as is(new figure)
+            }
+
+        }
+        for (var i = 0; i < this.fieldCells.length; i++){
+            for(var j = 0; j < this.fieldCells[i].length; j++){
+                for(var k = 0; k < coordinatesToCheck.length; k++){
+                    if(this.fieldCells[coordinatesToCheck[k].y][coordinatesToCheck[k].x].active){
+                        this.paintFigure();
+                        //if(direction === 'down'){
+                        //    this.figure = new Figure();
+                        //    return false;
+                        //}
+                        return false;
+                    }
+                }
+            }
+        }
+        this.paintFigure();
+        return true;
+    };
+
+    FieldController.prototype.checkCollectedLines = function (){
+        var collectedRows = [];
+        for(var i = 0; i < this.fieldCells.length; i++){
+            var collected = true;
+            for(var j = 0; j < this.fieldCells[i].length; j++){
+                if(!this.fieldCells[i][j].active){
+                    collected = false;
+                    break;
+                }
+            }
+            if(collected){
+                collectedRows.push(i);
+            }
+        }
+        if(collectedRows.length == 1){
+            this.playerScore += 100;
+        }
+        else{
+            if(collectedRows.length == 2){
+                this.playerScore += 300;
+            }
+            else{
+                if(collectedRows.length == 3){
+                    this.playerScore += 500;
+                }
+                else{
+                    if(collectedRows.length == 4){
+                        this.playerScore += 1000;
+                    }
+                }
+            }
+        }
+        $('.scores>.value').html(this.playerScore);
+        $('.speed>.value').html(this.speedHimanize);
+        collectedRows.length > 0 ? this.removeCollectedRows(collectedRows) : this.removeCollectedRows();
+        if(this.playerScore > this.currentStage && this.playerScore < 100000){
+
+            this.speed--;
+            this.speedHimanize++;
+            this.currentStage += 10000
+        }
+    };
+
+    FieldController.prototype.removeCollectedRows = function (indexes){
+        if(!(indexes && indexes.length > 0)){console.log('No lines collected'); return;}
+
+        var row, column;
+        for(var i = 0; i < indexes.length; i++){
+            for(row = indexes[i]; row >= 0; row--){
+                if(row == 0){
+                    for(column = 0; column < this.fieldCells[row].length; column++){
+                        this.fieldCells[row][column].active = 0;
+                    }
+                }
+                else{
+                    for(column = 0; column < this.fieldCells[row].length; column++){
+                        this.fieldCells[row][column].active = this.fieldCells[row - 1][column].active;
+                    }
+                }
+            }
+        }
+        this.refreshField();
+    };
+
+    FieldController.prototype.handleKeyMoves = function(){
+        var self = this;
+        $('body').on('keydown', function(event){
+            if(event.keyCode == 37){
+                if(self.canFigureMove('left')){
+                    self.clearFigure();
+                    self.figure.move('left');
+                    self.paintFigure();
+                }
+            }
+            if(event.keyCode == 39){
+                if(self.canFigureMove('right')){
+                    self.clearFigure();
+                    self.figure.move('right');
+                    self.paintFigure();
+                }
+            }
+            if(event.keyCode == 40){
+                if(self.canFigureMove('down')){
+                    self.clearFigure();
+                    self.figure.move('down');
+                    self.paintFigure();
+                }
+                else{
+                    self.checkCollectedLines();
+                    self.refreshField();
+                    self.figure = new Figure();
+                    self.paintFigure();
+                }
+
+            }
+            if(event.keyCode == 38){
+                self.clearFigure();
+                self.figure.rotate();
+                self.paintFigure();
+            }
+
+        });
+    };
+
+    FieldController.prototype.gameCycleStart = function (){
+        var self = this;
+        setInterval(function(){
+            self.speedCounter++;
+            if(self.speedCounter < self.speed){
+                self.speedCounter++;
+                return;
+            }
+            self.speedCounter = 0;
+            if(self.canFigureMove('down')){
+                self.clearFigure();
+                self.figure.move('down');
+                self.paintFigure();
+                self.refreshField();
+
+            }
+            else{
+                self.checkCollectedLines();
+                self.refreshField();
+                self.figure = new Figure();
+                self.paintFigure();
+            }
+        }, 100);
     };
 
     return FieldController;
--- js/figure.js
+++ js/figure.js
@@ -1,51 +1,72 @@
 define(['jquery'],function($){
-    var FIGURE_PRIMITIVES = {
-        square : {
-            topLeft : {x : 4, y : 0},
-            points : [
-                [1, 1],
-                [1, 1]
-            ]
-        },
 
-        line : {
-            topLeft : {x : 3, y : 0},
-            points : [
-                [0, 1, 0, 0],
-                [0, 1, 0, 0],
-                [0, 1, 0, 0],
-                [0, 1, 0, 0]
-            ]
-        },
-        trident : {
-            topLeft : {x : 3, y : 0},
-            points : [
-                [0, 1, 0],
-                [0, 1, 1],
-                [0, 1, 0]
-            ]
-        },
-        leftUpBlock : {
-            topLeft : {x : 4, y : 0},
-            points : [
-                [1, 0, 0],
-                [1, 1, 0],
-                [0, 1, 0]
-            ]
-
-        },
-        rightUpBlock : {
-            topLeft : {x : 4, y : 0},
-            points : [
-                [0, 1, 0],
-                [1, 1, 0],
-                [1, 0, 0]
-            ]
-        }
-
-    };
     function Figure(){
-        this.figure = FIGURE_PRIMITIVES.trident;
+        var FIGURE_PRIMITIVES = {
+            square : {
+                topLeft : {x : 4, y : 0},
+                points : [
+                    [1, 1],
+                    [1, 1]
+                ]
+            },
+
+            line : {
+                topLeft : {x : 3, y : 0},
+                points : [
+                    [0, 1, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 1, 0, 0],
+                    [0, 1, 0, 0]
+                ]
+            },
+            trident : {
+                topLeft : {x : 3, y : 0},
+                points : [
+                    [0, 1, 0],
+                    [0, 1, 1],
+                    [0, 1, 0]
+                ]
+            },
+            leftUpBlock : {
+                topLeft : {x : 4, y : 0},
+                points : [
+                    [1, 0, 0],
+                    [1, 1, 0],
+                    [0, 1, 0]
+                ]
+
+            },
+            rightUpBlock : {
+                topLeft : {x : 4, y : 0},
+                points : [
+                    [0, 1, 0],
+                    [1, 1, 0],
+                    [1, 0, 0]
+                ]
+            },
+            leftLine :{
+                topLeft : {x : 3, y : 0},
+                points : [
+                    [1, 1, 0],
+                    [0, 1, 0],
+                    [0, 1, 0]
+                ]
+            },
+            rightLine :{
+                topLeft : {x : 3, y : 0},
+                points : [
+                    [0, 1, 1],
+                    [0, 1, 0],
+                    [0, 1, 0]
+                ]
+            }
+
+
+        };
+
+        var primitiveNames = ['square', 'line', 'trident', 'leftUpBlock', 'rightUpBlock', 'leftLine', 'rightLine'];
+        var figurePrimitiveIndex = Math.round(Math.random() * 6);
+        this.figure = FIGURE_PRIMITIVES[primitiveNames[figurePrimitiveIndex]];
         this.rotateMatrix = new Array(this.figure.points.length);
         for(var i = 0; i < this.rotateMatrix.length; i++){
             this.rotateMatrix[i] = new Array(this.figure.points.length);
@@ -57,12 +78,6 @@ define(['jquery'],function($){
 
     Figure.prototype.rotate = function(){
         var length = this.figure.points.length;
-        //for(var i = 0; i < this.figure.points.length; i++){
-        //    for(var j = 0; j < this.figure.points.length; j++){
-        //        this.rotateMatrix[j][i] = this.figure.points[i][j];
-        //    }
-        //}
-        //console.dir(this.rotateMatrix);
         for(var i=0;i<length;i++){
             for(var j=0;j<length;j++){
                 this.rotateMatrix[j][length-1-i] = this.figure.points[i][j];
@@ -74,8 +89,6 @@ define(['jquery'],function($){
             }
         }
 
-        //console.dir(this.rotateMatrix);
-        //TNEW[j][n-1-i]=TOLD[i][j];
     };
 
     Figure.prototype.getCoordinates = function(){
@@ -93,6 +106,24 @@ define(['jquery'],function($){
             }
         }
         return coordinates;
+    };
+    /**
+     * @param direction Move direction, accepts string 'left'|'right'|'down'
+     */
+    Figure.prototype.move = function move(direction){
+        switch(direction){
+            case 'left' :
+                this.figure.topLeft.x--;
+                break;
+            case 'right' :
+                this.figure.topLeft.x++;
+                break;
+            case 'down' :
+            //fall through
+            default:
+                this.figure.topLeft.y++;
+
+        }
     };
     return Figure;
 
